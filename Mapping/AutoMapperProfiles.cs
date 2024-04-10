a9cc91478ef8dd9f028dd407a5db98b312f4ba35
kellylin <ms0521976@gmail.com>
2024-04-11 00:25:24 +0800

    AutoMapper

---
+++ Mapping/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using NorthWind.Models.Domain;
+using NorthWind.Models.Dto;
+
+namespace NorthWindProject.Mappings
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<CustomerDto, Customer>().ReverseMap();
+            CreateMap<UpdateCustomerDto, Customer>().ReverseMap();
+        }
+    }
+}
---
+++ Models/Dto/UpdateCustomerDto.cs
@@ -0,0 +1,23 @@
+﻿namespace NorthWind.Models.Dto
+{
+    public class UpdateCustomerDto
+    {
+        public string? ContactName { get; set; }
+
+        public string? ContactTitle { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Region { get; set; }
+
+        public string? PostalCode { get; set; }
+
+        public string? Country { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Fax { get; set; }
+    }
+}
